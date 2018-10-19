fa95ab99fb0562f559fdb6757a7bf777369e26f6
Chiraag Bangera <chiraag.bangera@live.com>
2018-10-19 17:01:22 -0500

    Pong Works!!

---
+++ public/javascripts/objects.js
@@ -0,0 +1,139 @@
+class Arena {
+	constructor(offset, border = false) {
+		this.offset = offset;
+		this.border = border;
+	}
+
+	rescale() {
+		this.x = Width / 2;
+		this.y = Height / 2;
+		this.w = Width - this.offset * 2;
+		this.h = Height - this.offset * 2;
+		this.xMin = this.offset;
+		this.xMax = this.offset + this.w;
+		this.yMin = this.offset;
+		this.yMax = this.offset + this.h;
+		this.centerX = (this.xMin + this.xMax) / 2;
+		this.centerY = (this.yMin + this.yMax) / 2;
+	}
+
+	draw() {
+		if (this.border) {
+			stroke(255, 0, 0);
+			strokeWeight(1);
+			fill(125);
+			line(this.xMin, this.y, this.xMax, this.y);
+			line(this.x, this.yMin, this.x, this.yMax);
+			noFill();
+			rect(this.x, this.y, this.w, this.h);
+		}
+	}
+}
+
+
+class Paddle {
+	constructor(id, arena, speed) {
+		this.id = id;
+		this.speed = speed;
+		this.arena = arena;
+	}
+
+	draw() {
+		stroke(255);
+		strokeWeight(1);
+		fill(255);
+		rect(this.x, this.y, PADDLE_THICKNESS, PADDLE_LENGTH);
+	}
+
+	rescale() {
+		switch (this.id) {
+			case 0: {
+				this.x = this.arena.xMin + PADDLE_THICKNESS / 2;
+				this.y = this.arena.centerY;
+			} break;
+			case 1: {
+				this.x = arena.xMax - PADDLE_THICKNESS / 2;
+				this.y = arena.centerY;
+
+			} break;
+		}
+		this.computeBoundaries();
+	}
+
+	computeBoundaries() {
+		this.lMin = this.y - PADDLE_LENGTH / 2;
+		this.lMax = this.y + PADDLE_LENGTH / 2;
+		this.tMin = this.x - PADDLE_THICKNESS / 2;
+		this.tMax = this.x + PADDLE_THICKNESS / 2;
+	}
+
+	move(x, y) {
+		switch (this.id) {
+			case 0:
+			case 1: {
+				this.y += y * this.speed;
+				this.y = constrain(this.y, arena.yMin + PADDLE_LENGTH / 2, arena.yMax - PADDLE_LENGTH / 2);
+			}; break;
+		}
+		this.computeBoundaries();
+	}
+}
+
+
+
+class Ball {
+	constructor(x, y, size, dx, dy) {
+		this.x = x;
+		this.y = y;
+		this.dx = dx;
+		this.dy = dy;
+		this.size = size;
+		this.radius = size / 2;
+	}
+
+	move() {
+		this.x += this.dx;
+		this.y += this.dy;
+	}
+
+	draw() {
+		strokeWeight(1);
+		stroke(255);
+		fill(255);
+		ellipse(this.x, this.y, this.radius, this.radius);
+	}
+
+	physX(arena, paddles = null) {
+
+        if (this.x - this.radius / 2 <= arena.xMin || this.x + this.radius / 2 >= arena.xMax) {
+			return;
+		}
+
+		paddles.forEach(paddle => {
+			switch (paddle.id) {
+				case 0: {
+					if (this.y - this.radius / 2 > paddle.lMin && this.y + this.radius / 2 < paddle.lMax) {
+						if (this.x - this.radius / 2 <= paddle.tMax) {
+							this.dx = -this.dx;
+						}
+					}
+				}; break;
+				case 1: {
+					if (this.y - this.radius / 2 > paddle.lMin && this.y - this.radius / 2 < paddle.lMax) {
+						if (this.x + this.radius / 2 >= paddle.tMin) {
+							this.dx = -this.dx;
+						}
+					}
+				}; break;
+			}
+        });
+        
+		if (this.y - this.radius / 2 <= arena.yMin) {
+			this.dy = -this.dy;
+		}
+
+		if (this.y + this.radius / 2 >= arena.yMax) {
+			this.dy = -this.dy;
+		}
+	}
+}
--- public/javascripts/pong.js
+++ public/javascripts/pong.js
@@ -30,25 +30,28 @@ function setup() {
 	let h = canvasHolder.offsetHeight;
 	gameCanvas = createCanvas(w, h);
 	gameCanvas.parent(canvasHolder);
-	rescale();
 	textAlign(CENTER, CENTER);
 	ellipseMode(CENTER);
 	rectMode(CENTER);
 	angleMode(DEGREES);
 	frameRate(60);
-	arena = new Arena(10, true);
-	ball = new Ball(arena.centerX, arena.centerY, 50, -2, 0);
-	paddles.push(new Paddle(0, arena.xMin, arena.centerY, 5));
+	arena = new Arena(1);
+	paddles.push(new Paddle(0, arena, 5));
+	paddles.push(new Paddle(1, arena, 5));
+	rescale();
 }
 
 function rescale() {
 	Width = gameCanvas.width;
 	Height = gameCanvas.height;
-	scale = 0.5;
-	paddleThickness = 5 / scale;
 	fSize = 100;
 	if (arena) {
 		arena.rescale();
+	}
+	if (paddles) {
+		paddles.forEach(paddle => {
+			paddle.rescale();
+		});
 	}
 }
 
@@ -60,10 +63,23 @@ function windowResized() {
 	rescale();
 }
 
+function handleInput() {
+	if (keyIsPressed) {
+		if (keyCode === UP_ARROW) {
+			paddles[ID].move(0, -1);
+			paddles[1].move(0, -1);
+		}
+		else if (keyCode === DOWN_ARROW) {
+			paddles[ID].move(0, 1);
+			paddles[1].move(0, 1);
+		}
+	}
+}
+
 
 
 function draw() {
-	background(51);
+	background(0);
 	drawFrame();
 }
 
@@ -71,153 +87,21 @@ function draw() {
 
 function drawFrame() {
 	arena.draw();
+	handleInput();
 	paddles.forEach(paddle => {
 		paddle.draw();
 	});
-	ball.move();
-	ball.physX(arena, paddles);
-	ball.draw();
-}
-
-
-class Arena {
-	constructor(offset, border = false) {
-		this.offset = offset;
-		this.x = Width / 2;
-		this.y = Height / 2;
-		this.w = Width - this.offset * 2;
-		this.h = Height - this.offset * 2;
-		this.border = border;
-		this.xMin = this.offset;
-		this.xMax = this.offset + this.w;
-		this.yMin = this.offset;
-		this.yMax = this.offset + this.h;
-		this.centerX = (this.xMin + this.xMax) / 2;
-		this.centerY = (this.yMin + this.yMax) / 2;
+	if(ball){
+		ball.move();
+		ball.physX(arena, paddles);
+		ball.draw();
 	}
-
-	rescale() {
-		this.x = Width / 2;
-		this.y = Height / 2;
-		this.w = Width - this.offset * 2;
-		this.h = Height - this.offset * 2;
-		this.xMin = this.offset;
-		this.xMax = this.offset + this.w;
-		this.yMin = this.offset;
-		this.yMax = this.offset + this.h;
-		this.centerX = (this.xMin + this.xMax) / 2;
-		this.centerY = (this.yMin + this.yMax) / 2;
-	}
-
-	draw() {
-		if (this.border) {
-			stroke(255, 0, 0);
-			strokeWeight(1);
-		} else {
-			stroke(0);
-			strokeWeight(1);
-		}
-		fill(125);
-		line(this.xMin, this.y, this.xMax, this.y);
-		line(this.x, this.yMin, this.x, this.yMax);
-		noFill();
-		rect(this.x, this.y, this.w, this.h);
+	else{
+		ball = new Ball(arena.centerX, arena.centerY, 50, random(-10,10),random(-10,10));
 	}
 }
 
-
-class Paddle {
-	constructor(id, x, y, speed) {
-		this.id = id;
-		this.x = x + PADDLE_THICKNESS / 2;
-		this.y = y;
-		this.lMin = Height - this.y - PADDLE_LENGTH;
-		this.lMax = Height - this.y + PADDLE_LENGTH;
-		this.speed = speed;
-	}
-
-	draw() {
-		stroke(255);
-		strokeWeight(1);
-		rect(this.x, this.y, PADDLE_THICKNESS, PADDLE_LENGTH);
-	}
-
-	move(x, y) {
-		switch (this.id) {
-			case 0: {
-
-			}; break;
-			case 1: {
-
-			}; break;
-			case 2: {
-
-			}; break;
-			case 3: {
-
-			};
-		}
-	}
+function random(min,max){
+	return Math.random()*max + min;
 }
 
-
-
-class Ball {
-	constructor(x, y, size, dx, dy) {
-		this.x = x;
-		this.y = y;
-		this.dx = dx;
-		this.dy = dy;
-		this.size = size;
-		this.radius = size / 2;
-	}
-
-	move() {
-		this.x += this.dx;
-		this.y += this.dy;
-	}
-
-	draw() {
-		strokeWeight(1);
-		stroke(255, 0, 0);
-		fill(125);
-		ellipse(this.x, this.y, this.radius, this.radius);
-	}
-
-	physX(arena, paddles = null) {
-
-		paddles.forEach(paddle => {
-			switch (paddle.id) {
-				case 0: {
-				}; break;
-				case 1: {
-
-				}; break;
-				case 2: {
-
-				}; break;
-				case 3: {
-
-				};
-			}
-		});
-
-
-
-		if (this.x - this.radius / 2 <= arena.xMin) {
-			this.dx = -this.dx;
-		}
-
-		if (this.x + this.radius / 2 >= arena.xMax) {
-			this.dx = -this.dx;
-		}
-
-		if (this.y - this.radius / 2 <= arena.yMin) {
-			this.dy = -this.dy;
-		}
-
-		if (this.y + this.radius / 2 >= arena.yMax) {
-			this.dy = -this.dy;
-		}
-	}
-}
